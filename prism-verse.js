4605d2cc453c149dc23ef095ba40190150153654
Mobi Fruit <mobifruit@users.noreply.github.com>
2026-02-02 22:07:35 +0100

    Update prism-verse.js
    Adds specifiers and types to the language template

--- prism-verse.js
+++ prism-verse.js
@@ -18,8 +18,38 @@ Prism.languages.verse = {
     greedy: true
   },
 
+  'visibility-specifier': {
+    pattern: /<\s*(?:public|internal|protected|private)\s*>/,
+    greedy: true,
+    alias: 'keyword'
+  },
+
+  'class-specifier': {
+    pattern: /<\s*(?:concrete|unique|final|persistable)\s*>/,
+    greedy: true,
+    alias: 'keyword'
+  },
+
+  'effect-specifier': {
+    pattern: /<\s*(?:converges|decides)\s*>/,
+    greedy: true,
+    alias: 'keyword'
+  },
+
+  'enum-specifier': {
+    pattern: /<\s*(?:open|closed)\s*>/,
+    greedy: true,
+    alias: 'keyword'
+  },
+
+  'function-specifier': {
+    pattern: /<\s*(?:constructor)\s*>/,
+    greedy: true,
+    alias: 'keyword'
+  },
+  
   // Keywords (core Verse structure)
-  'keyword': /\b(module|class|interface|struct|enum|event|if|else|for|while|loop|return|break|continue|var|const|set|function|using|import|public|private|protected|override|abstract|native)\b/,
+  'keyword': /\b(module|event|profile|block|if|else|case|loop|return|break|for|defer|continue|var|set|function|using|import|override|abstract|native|pure)\b/,
 
   // Boolean literals
   'boolean': /\b(true|false)\b/,//Optionally: null|undefined
@@ -29,6 +59,17 @@ Prism.languages.verse = {
 
   // Types (lowerCamelCase before parentheses)
   'class-name': /\b[a-z_][A-Za-z0-9_]*(?=\s*\()/,///\b[A-Z][A-Za-z0-9_]*\b/
+
+  'common-types': /\b(logic|int|float|string|rational|any|comparable|void)\b/,
+
+  'container-types': /\b(option|array|map)\b/,
+
+  'composite-types': /\b(class|enum|struct|interface|constructor)\b/,
+
+  //<override>|<decides>|<constructor> (special function)
+
+  //Visibility Specifiers: <public>|<internal>|<protected>|<private>
+  //Effects / Effect specifiers: <converges>
 
   // Functions (FirstCaps)
   'function': /\b[A-Z][A-Za-z0-9_]*\b/,///\b[a-z_][A-Za-z0-9_]*(?=\s*\()/
